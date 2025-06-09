2bc0879d34bd9047333ad8a6f6e1e86c0b612199
Cetecom <jua.tapiao@duocuc.cl>
2025-06-09 16:54:40 -0400

    Version .5
    Creacionde todo el user

--- src/main/java/com/example/officialecomarketspa/controller/UsuarioController.java
+++ src/main/java/com/example/officialecomarketspa/controller/UsuarioController.java
@@ -1,4 +1,31 @@
 package com.example.officialecomarketspa.controller;
 
+import com.example.officialecomarketspa.model.Usuario;
+import com.example.officialecomarketspa.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("api/usuarios")
 public class UsuarioController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> findAll(){
+        return ResponseEntity.ok(userService.obtenerTodosLosUsuarios());
+    }
+
+    @PostMapping()
+    public ResponseEntity<Usuario> guardarUsuario(@RequestBody Usuario usuario){
+        return ResponseEntity.ok(userService.guardarUsuario(usuario));
+    }
 }
